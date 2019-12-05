e4d024d147dbdbd90771a21aaa9cf94819908ca0
RichaVPatel <richu2593@gmail.com>
2019-12-05 10:44:03 -0500

    Hello world

---
+++ src/main/java/com/hello/helloword/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.hello.helloword;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World !!";
+    }
+}
